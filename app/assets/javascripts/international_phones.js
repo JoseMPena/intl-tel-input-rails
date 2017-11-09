753a50828f4fe6c9cc08de94c0b251c4c85ba601
josempnena <jose.m.pena.diaz@gmail.com>
2017-11-09 18:24:35 +0100

    Fix country load on tab change

---
+++ app/assets/javascripts/international_phones.js
@@ -0,0 +1,15 @@
+//= require intlTelInput
+//= require phone_utils
+
+$(function() {
+    $('[data-intl-tel-input]').each(function () {
+        var element = $(this);
+        var hidden = element.parent().siblings('.international_phone_number');
+        element.intlTelInput(element.data('intl-tel-input') || {});
+
+        element
+            .closest('form').submit(function () {
+            hidden.val(element.intlTelInput('getNumber').replace(/(.*\+)(.*?)(?=\+)/g));
+        });
+    });
+});
